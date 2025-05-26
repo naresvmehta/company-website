f6fc594b41b1ffe1fcb9e3191cc6ae753c2b2e2b
naresvmehta <naresvmehta@gmail.com>
2025-05-26 18:11:15 +0530

    Allow external connections

--- server.js
+++ server.js
@@ -79,8 +79,7 @@ app.post('/submit', async (req, res) => {
   }
 });
 
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
+
